3dd228515b8b8f602b36cadfe84b1e9860b6ec67
Wesley Bosse <WBosse@san-mac4109.merchant.local>
2017-07-13 18:24:24 -0700

    now we're cooking with gas

--- adminProvision.pl
+++ adminProvision.pl
@@ -9,7 +9,8 @@ sub xml_upload {
 	my ($host, $user, $pass, $dir, $fpath) = @_;
 	my $ftp;
 
-	$ftp = Net::FTP->new($host, Debug => 0);
+
+	$ftp = Net::FTP->new($host, Debug => 1);
 	$ftp->login($user, $pass) || die $ftp->message;
 	$ftp->cwd($dir);
 	$ftp->put($fpath) || die $ftp->message;
@@ -19,10 +20,11 @@ sub xml_upload {
 }
 
 sub password_creation {
-		my $sel = WWW::Selenium->new( host => "localhost",
-		port => 4444,
-		browser => "*iexplore",
-		browser_url => "http://www.google.com",
+		my $sel = WWW::Selenium->new( 	
+			host => "localhost",
+			port => 4444,
+			browser => "*iexplore",
+			browser_url => "http://www.google.com",
 		);
 
 	$sel->start;
@@ -33,3 +35,27 @@ sub password_creation {
 	print $sel->get_title;
 	$sel->stop;
 }
+
+print "FTP Host:\n";
+my $input_ftp_host = <STDIN>;
+print "FTP Username:\n";
+my $input_ftp_user = <STDIN>;
+print "FTP Password:\n";
+my $input_ftp_pass = <STDIN>;
+print "Dev Key:\n";
+my $input_dev_key = <STDIN>;
+print "Dev Account Password:\n";
+my $input_dev_pass = <STDIN>;
+
+my $pre_provide_file_location = '/Users/WBosse/Desktop/merica.png';
+my $destination_directory = '/htdocs/mm5/test'
+
+chomp($input_ftp_host,$input_ftp_user,$input_ftp_pass,$input_dev_key,$input_dev_pass);
+
+xml_upload(
+	$input_ftp_host,
+	$input_ftp_user, 
+	$input_ftp_pass,
+	$destination_directory,
+	$pre_provide_file_location
+);
