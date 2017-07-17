7ffcf1d6f5d8708965e385e6c049d7eeaefece11
Wesley Bosse <wes.bosse@gmail.com>
2017-07-16 23:01:46 -0700

    just givin the people what they want
    Please enter the commit message for your changes. Lines starting

--- adminProvision.pl
+++ adminProvision.pl
@@ -3,9 +3,20 @@ use strict;
 use warnings;
 
 use XML::Simple;
+use Data::Dumper;
 use Net::FTP;
 use WWW::Mechanize;
 
+sub pre_provide_manipulation {
+	# create object
+	$xml = new XML::Simple;
+
+	# read XML file
+	$pre_provide = $xml->XMLin("pre-provide.xml");
+
+	# print output
+	print Dumper($pre_provide);
+}
 
 sub xml_upload {
 	my ($host, $user, $pass, $dir, $fpath) = @_;
@@ -21,15 +32,17 @@ sub xml_upload {
 	print $ftp->message;
 }
 
+
+
 sub password_creation {
-	my $mech = WWW::Mechanize->new;  
+ 	my $mech = WWW::Mechanize->new;  
 	my $sequence = '...';
 	# $mech->get('https://'.$sub.$name'.com/Merchant2/admin.mvc');
-	$mech->get('');
+	$mech->get('https://dts3100.mivamerchantdev.com/mm5/admin.mvc');
 	$mech->submit_form(
 		form_number => 0,
 		fields => {
-			'UserName' => '',
+			'UserName' => 'wbosse@miva.com',
 			'PassWord' => '',
 		},
 	);
@@ -47,34 +60,56 @@ sub password_creation {
 	print $mech->content;
 }
 
-sub user_input{
-	print "FTP Host:\n";
-	my $input_ftp_host = <STDIN>;
-	print "FTP Username:\n";
-	my $input_ftp_user = <STDIN>;
-	print "FTP Password:\n";
-	my $input_ftp_pass = <STDIN>;
-	print "Dev Key:\n";
-	my $input_dev_key = <STDIN>;
-	print "Dev Account Password:\n";
-	my $input_dev_pass = <STDIN>;
-
-	my $pre_provide_file_location = '/Users/WBosse/Desktop/merica.png';
-	my $destination_directory = '/htdocs/mm5/test';
-
-	chomp($input_ftp_host,$input_ftp_user,$input_ftp_pass,$input_dev_key,$input_dev_pass);
-}
 
 
-# xml_upload(
-# 	$input_ftp_host,
-# 	$input_ftp_user, 
-# 	$input_ftp_pass,
-# 	$destination_directory,
-# 	$pre_provide_file_location
-# );
+# Testing Variables
+my $pre_provide_file_location = '/Users/WBosse/Desktop/merica.png';
+my $destination_directory = '/mivadata/mm5/test';
 
-password_creation();
-# user_input();
 
-# handle merchant2 vs mm5
+
+# Gather User Input
+print "FTP Host:\n";
+my $input_ftp_host = <STDIN>;
+print "FTP Username:\n";
+my $input_ftp_user = <STDIN>;
+print "FTP Password:\n";
+my $input_ftp_pass = <STDIN>;
+print "Dev Key:\n";
+my $input_dev_key = <STDIN>;
+print "Dev Account Password:\n";
+my $input_dev_pass = <STDIN>;
+print "Domain:\n";
+my $input_domain = <STDIN>;
+
+
+
+chomp(
+		$input_ftp_host,
+		$input_ftp_user,
+		$input_ftp_pass,
+		$input_dev_key,
+		$input_dev_pass,
+		$input_domain
+	);
+
+xml_upload(
+		$input_ftp_host,
+		$input_ftp_user, 
+		$input_ftp_pass,
+		$destination_directory,
+		$pre_provide_file_location
+	);
+
+password_creation(
+		$input_domain
+	);
+
+
+
+
+
+#TODO:
+	# handle merchant2 vs mm5
+	# user input converted to userConfig.xml
+	# create account for all sub-domains
